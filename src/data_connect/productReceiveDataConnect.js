ffcb3be533ed413050a0b3fdf167174920869647
fkakd1205 <fkakd0102@naver.com>
2022-11-28 14:38:59 +0900

    REFACTOR : product release, product receive 사용하지 않는 api 제거

--- src/data_connect/productReceiveDataConnect.js
+++ src/data_connect/productReceiveDataConnect.js
@@ -4,24 +4,6 @@ const API_SERVER_ADDRESS = process.env.REACT_APP_API_HOST;
 
 const productReceiveDataConnect = () => {
     return {
-        postList: async function(data){
-            return await axios.post(`${API_SERVER_ADDRESS}/api/v1/product-receive/list`, data,{
-                withCredentials: true
-            })
-        },
-        searchList: async function(productOptionCid) {
-            return await axios.get(`${API_SERVER_ADDRESS}/api/v1/product-receive/list/${productOptionCid}`,{
-                withCredentials: true
-            })
-        },
-        // memo 수정 <- TODO::path로 변경해도 될듯
-        putOne: async function (data) {
-            return await axios.put(`${API_SERVER_ADDRESS}/api/v1/product-receive/one`, data, {
-                withCredentials: true
-            })
-        },
-        
-        // [221025] FEAT
         createBatch: async function(data){
             return await axios.post(`${API_SERVER_ADDRESS}/api/v2/product-receive/batch`, data,{
                 withCredentials: true
--- src/data_connect/productReleaseDataConnect.js
+++ src/data_connect/productReleaseDataConnect.js
@@ -4,28 +4,11 @@ const API_SERVER_ADDRESS = process.env.REACT_APP_API_HOST;
 
 const productReleaseDataConnect = () => {
     return {
-        postList: async function(data){
-            return await axios.post(`${API_SERVER_ADDRESS}/api/v1/product-release/list`, data,{
-                withCredentials: true
-            })
-        },
-        searchList: async function(productOptionCid) {
-            return await axios.get(`${API_SERVER_ADDRESS}/api/v1/product-release/list/${productOptionCid}`,{
-                withCredentials: true
-            })
-        },
-        putOne: async function (data) {
-            return await axios.put(`${API_SERVER_ADDRESS}/api/v1/product-release/one`, data, {
-                withCredentials: true
-            })
-        },
         searchOneByErpOrderItemId: async function (orderItemId) {
             return await axios.get(`${API_SERVER_ADDRESS}/api/v1/product-release/erp-order-item/${orderItemId}`,{
                 withCredentials: true
             })
         },
-
-        // [221026] FEAT
         createBatch: async function(data){
             return await axios.post(`${API_SERVER_ADDRESS}/api/v2/product-release/batch`, data,{
                 withCredentials: true
