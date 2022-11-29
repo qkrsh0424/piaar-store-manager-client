4a83cd763e028ebbcacc5121b77e82428e01b498
fkakd1205 <fkakd0102@naver.com>
2022-11-29 17:10:56 +0900

    FIX : 세트상품은 옵션패키지 등록 시 구성상품으로 등록할 수 없도록 함

--- src/component/product-management/product-manage-v2/modal/option-package-modal/hooks/useProductOptionsSearchHook.js
+++ src/component/product-management/product-manage-v2/modal/option-package-modal/hooks/useProductOptionsSearchHook.js
@@ -8,7 +8,10 @@ export default function useProductOptionsSearchHook () {
         await productOptionDataConnect().searchAllRelatedProduct()
             .then(res => {
                 if (res.status === 200 && res.data && res.data.message === 'success') {
-                    setProductOptions(res.data.data);
+                    // 패키지상품으로 등록된 애들은 제외하고 반환
+                    // 패키지상품은 패키지구성상품 등록 제한
+                    let unpakcageOptions = (res.data.data).filter(r => r.option.packageYn === 'n');
+                    setProductOptions(unpakcageOptions);
                 }
             })
             .catch(err => {
